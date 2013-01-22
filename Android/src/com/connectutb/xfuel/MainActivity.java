feaefb20ee185a4c5972c6d3dca9cd6ba602ba9e
Untouchab1e <edvard.holst@gmail.com>
2013-01-22 10:00:58 +0100

    Added preferences and made improvements to main layout.

--- Android/src/com/connectutb/xfuel/MainActivity.java
+++ Android/src/com/connectutb/xfuel/MainActivity.java
@@ -3,13 +3,23 @@ package com.connectutb.xfuel;
 import java.util.ArrayList;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
+	/* Our preferences */
+	public SharedPreferences settings;
+	public SharedPreferences.Editor editor;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,6 +42,12 @@ public class MainActivity extends Activity {
 	    		R.layout.spinner_white, aircraftArray);
 	    spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 	    localSpinner.setAdapter(spinnerArrayAdapter);
+	    
+	    //Init prefs
+	    settings = PreferenceManager.getDefaultSharedPreferences(this);
+	    
+	    //Grab Default values
+	    grabDefaults();
 	}
 
 	@Override
@@ -40,5 +56,33 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
+	
+	public void grabDefaults(){
+		RadioButton radioMetrics = (RadioButton)findViewById(R.id.radioMetrics);
+		RadioButton radioImperial = (RadioButton)findViewById(R.id.radioImperial);
+		Log.d("XFUEL", settings.getString("def_units", "Metrics"));
+		if (settings.getString("def_units", "Metrics").equals("Metrics")){
+				radioMetrics.setChecked(true);
+				
+		}else{
+			radioImperial.setChecked(true);
+		}
+		
+	}
+	
+	/* Action on menu selection */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	switch(item.getItemId()){
+    	//Open prefs
+    	case R.id.menu_settings:
+    		Intent i = new Intent(this, Preferences.class);
+        	startActivity(i);	 
+    		return true;
+    		
+    	default:
+    		return super.onOptionsItemSelected(item);
+    	}
+    }
+    
 }
