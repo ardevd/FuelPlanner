0bae5f7eac1de853b66e1e45302f5961a49d4475
Untouchab1e <edvard.holst@gmail.com>
2013-01-22 11:47:21 +0100

    Implemented HTTP post method.

--- Android/src/com/connectutb/xfuel/FuelPlanner.java
+++ Android/src/com/connectutb/xfuel/FuelPlanner.java
@@ -1,8 +1,16 @@
 package com.connectutb.xfuel;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 
@@ -13,11 +21,38 @@ public class FuelPlanner {
 	private final HttpPost httppost;
 	private final HttpClient httpclient;
 	
-	public FuelPlanner (Activity context){
+	private final String aircraft;
+	
+	
+	public FuelPlanner (Activity context, String aircraft){
 		/* Assign variables */
 		this.context = context;
 		this.httppost = new HttpPost(context.getResources().getString(R.string.post_url));
 		this.httpclient = new DefaultHttpClient();
+		this.aircraft = aircraft;
 	}
 
+	public void submitFuelRequest(){
+		
+		/** Post fuel data to server **/
+		new Thread(new Runnable(){
+			public void run(){
+				try{
+					//add data
+					List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+					nameValuePairs.add(new BasicNameValuePair("aircraft", aircraft));
+					
+					httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+					
+					//Execute HTTP Post
+					httpclient.execute(httppost);
+					
+				}catch(ClientProtocolException e){
+					//TODO catch block
+				}catch(IOException e){
+					//TODO: catch block
+				}
+			}
+		}).start();
+	}
 }
