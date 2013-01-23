31e494041e24b3ed2c1983c3409ad3b15b927add
Untouchab1e <edvard.holst@gmail.com>
2013-01-23 15:20:16 +0100

    Added ShareActionProvider and implemented functionality to share the Load sheet

--- Android/src/com/connectutb/xfuel/FuelReport.java
+++ Android/src/com/connectutb/xfuel/FuelReport.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 public class FuelReport extends ListActivity{
@@ -17,6 +19,8 @@ public class FuelReport extends ListActivity{
 	private String aircraft;
 	private String origin;
 	private String destination;
+	
+	private ShareActionProvider mShareActionProvider;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
@@ -42,6 +46,44 @@ public class FuelReport extends ListActivity{
 	    setListAdapter(new FuelReportListAdapter(this, fuelData));
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate menu resource file.
+	    getMenuInflater().inflate(R.menu.fuel_report, menu);
+
+	    // Locate MenuItem with ShareActionProvider
+	    MenuItem item = menu.findItem(R.id.menu_item_share);
+
+	    // Fetch and store ShareActionProvider
+	    mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+	    
+	    mShareActionProvider.setShareIntent(createShareIntent());
+	    // Return true to display menu
+	    return true;
+	}
+	
+	private Intent createShareIntent(){
+		/** Compile a nicely formatted load sheet **/
+		String load_sheet = "xFuel Load Sheet" + System.getProperty("line.separator");
+		load_sheet += origin + "->" + destination + " (" + aircraft + ")" + System.getProperty("line.separator");
+		for (int i = 0; i < getListView().getLastVisiblePosition(); i++){
+			Object o = getListAdapter().getItem(i);
+	    	Log.d("XFUEL", o.toString());
+	    	load_sheet += o.toString().replace("-", ":") + System.getProperty("line.separator");
+		}
+		Intent I= new Intent(Intent.ACTION_SEND);
+        I.setType("text/plain");
+        I.putExtra(android.content.Intent.EXTRA_SUBJECT, "xFuel - Fuel Plan: " + origin + "->" + destination + " (" + aircraft + ")");
+        I.putExtra(android.content.Intent.EXTRA_TEXT, load_sheet);
+        return I;
+	}
+
+	// Call to update the share intent
+	private void setShareIntent(Intent shareIntent) {
+	    if (mShareActionProvider != null) {
+	        mShareActionProvider.setShareIntent(shareIntent);
+	    }
+	}
 	
 	/* Action on menu selection */
     @Override
