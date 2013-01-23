ae526badcd12b1a844e649dab8e0520b1d489ae4
Untouchab1e <edvard.holst@gmail.com>
2013-01-23 10:31:13 +0100

    Made a nice row layout for the FuelReport and finished the first draft of the listAdapter

--- Android/src/com/connectutb/xfuel/FuelReportListAdapter.java
+++ Android/src/com/connectutb/xfuel/FuelReportListAdapter.java
@@ -1,7 +1,11 @@
 package com.connectutb.xfuel;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class FuelReportListAdapter extends ArrayAdapter<String> {
 	private final Activity context;
@@ -13,6 +17,41 @@ public class FuelReportListAdapter extends ArrayAdapter<String> {
 		this.fuelData = fuelData;
 	}
 	
+	// static to save the reference to the outer class and to avoid access to
+		// any members of the containing class
+		static class ViewHolder {
+			public TextView textViewKey;
+			public TextView textViewValue;
+		}
 	
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// ViewHolder will buffer the assess to the individual fields of the row
+			// layout
+
+			ViewHolder holder;
+			// Recycle existing view if passed as parameter
+			// This will save memory and time on Android
+			// This only works if the base layout for all classes are the same
+			View rowView = convertView;
+			if (rowView == null) {
+				LayoutInflater inflater = context.getLayoutInflater();
+				rowView = inflater.inflate(R.layout.fuel_report_row, null, true);
+				rowView.setBackgroundColor(0x1a1a1a);
+				holder = new ViewHolder();
+				holder.textViewKey = (TextView) rowView.findViewById(R.id.textViewKey);
+				holder.textViewValue = (TextView) rowView.findViewById(R.id.textViewValue);
+				rowView.setTag(holder);
+			} else {
+				holder = (ViewHolder) rowView.getTag();
+			}
+			String[] arrayString = fuelData[position].split(";");
+			String fKey = arrayString[0];
+			String fValue = arrayString[1];
+			holder.textViewKey.setText(fKey);
+			holder.textViewValue.setText(fValue);
+			
+			return rowView;
+		}
 
 }
