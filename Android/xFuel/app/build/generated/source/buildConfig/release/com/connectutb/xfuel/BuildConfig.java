b276f668516bcd43e99a539dc2d15ee17228a8c5
Zyg0te <edvard.holst@gmail.com>
2015-03-16 10:14:12 +0100

    Converted project to Android Studio

---
+++ Android/xFuel/app/build/generated/source/buildConfig/release/com/connectutb/xfuel/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.connectutb.xfuel;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.connectutb.xfuel";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 24;
+  public static final String VERSION_NAME = "";
+}
