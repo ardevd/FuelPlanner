39f282142b8e943fa1d0b14c06f12f50463a1dda
Untouchab1e <edvard.holst@gmail.com>
2015-08-17 19:42:31 +0200

    Added initial iOS project

---
+++ iOS/xFuel/xFuel/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  xFuel
+//
+//  Created by Edvard Holst on 17/08/15.
+//  Copyright (c) 2015 Zygote Labs. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS/xFuel/xFuel/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  xFuel
+//
+//  Created by Edvard Holst on 17/08/15.
+//  Copyright (c) 2015 Zygote Labs. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
