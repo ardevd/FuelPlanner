d38e1a0911fd18767c18f7a98b4d2fe6172a0036
Zyg0te <edvard.holst@gmail.com>
2015-08-27 21:21:21 +0200

    Work on the new layout has begun

--- Android/xFuel/app/src/main/java/com/connectutb/xfuel/NavigationDrawerFragment.java
+++ Android/xFuel/app/src/main/java/com/connectutb/xfuel/NavigationDrawerFragment.java
@@ -247,7 +247,7 @@ public class NavigationDrawerFragment extends Fragment {
             return true;
         }
 
-        if (item.getItemId() == R.id.action_example) {
+        if (item.getItemId() == R.id.action_calculate) {
             Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             return true;
         }
