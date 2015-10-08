d303043962b6d624c141a0bfcb8e917cf5915de1
Zygote <edvard.holst@gmail.com>
2015-10-08 14:17:46 +0200

    Added more OutputMatcher items

---
+++ Android/xFuel/app/src/main/java/com/connectutb/xfuel/HistoryFragment.java
@@ -0,0 +1,11 @@
+package com.connectutb.xfuel;
+
+import android.app.Fragment;
+
+import com.connectutb.xfuel.providers.HistoryContract;
+
+/**
+ * Created by Zygote on 08.10.2015.
+ */
+public class HistoryFragment extends Fragment implements HistoryContract {
+}
--- Android/xFuel/app/src/main/java/com/connectutb/xfuel/tools/OutputMatcher.java
+++ Android/xFuel/app/src/main/java/com/connectutb/xfuel/tools/OutputMatcher.java
@@ -26,9 +26,21 @@ public class OutputMatcher {
             desc = context.getString(R.string.heading_tc_summary);
         } else if (key.equals("OEW")){
             title = context.getString(R.string.oew);
+            desc = context.getString(R.string.oew_summary);
         } else if (key.equals("TIME_BLK")){
             title = context.getString(R.string.time_blk);
             desc = context.getString(R.string.time_blk_summary);
+        } else if (key.equals("TIME_TTE")){
+            title = context.getString(R.string.time_tte);
+            desc = context.getString(R.string.time_tte_summary);
+        } else if (key.equals("FUEL_RSV")){
+            title = context.getString(R.string.fuel_rsv);
+            desc = context.getString(R.string.fuel_rsv_summary);
+        } else if (key.equals("TIME_RSV")){
+            title = context.getString(R.string.time_rsv);
+            desc = context.getString(R.string.time_rsv_summary);
+        } else if (key.equals("NM")){
+            desc = context.getString(R.string.nm_summary);
         }
 
         values.put("title", title);
